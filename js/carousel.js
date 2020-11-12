edc0d4a7ec9bb8cf60317dd379c15b41feb0494a
Pauline Momot <pauline.momot@gmail.com>
2020-11-12 17:57:46 +0100

    Site web fonctionnel

---
+++ js/carousel.js
@@ -0,0 +1,90 @@
+// Asynchronous script : add eventlistener
+console.log('coucou');
+class Carousel {
+
+    // PARAMETER
+    // {HTMLelement} element
+    // {object} options
+    // {object} options.slidesToScroll : number of element to scroll
+    // {object} options.slidesVisible : number of visible elements in a slide
+
+    constructor (element, options = {}) {
+        this.element = element;
+        this.options = Object.assign({}, {
+            slidesToScroll: 1,
+            slidesVisible: 1
+        }, options)
+        //Save children in a variable
+        //Keep children at the moment of the script execution
+        let children = [].slice.call(element.children);
+        //Save current slide
+        this.currentItem = 0
+        // create element DOM for carousel
+        this.root = this.createDivWithClass('carousel');
+        this.container = this.createDivWithClass('carousel__container');
+        this.root.appendChild(this.container);
+        this.element.appendChild(this.root)
+        // children in the container
+        this.items = children.map((child) => {
+            let item =this.createDivWithClass('carousel__item');
+            item.appendChild(child);
+            this.container.appendChild(item);
+            item.classList.add('d-flex', 'flex-column', 'align-items-center');
+            return item
+        });
+        this.setStyle();
+        this.createNavigation();
+    }
+
+
+    //Container item carousel
+    setStyle () {
+        let ratio = this.items.length / this.options.slidesVisible; // width container ratio calcul
+        this.container.style.width = (ratio * 100) + '%'; //width container
+        this.items.forEach(item => item.style.width = ((100 / this.options.slidesVisible) / ratio) + '%'        
+        );
+    }
+
+    createNavigation () {
+        let nextButton = this.createDivWithClass('carousel__next');
+        let prevButton = this.createDivWithClass('carousel__prev');
+        this.root.appendChild(nextButton);
+        this.root.appendChild(prevButton);
+        nextButton.addEventListener('click', this.next.bind(this));
+        prevButton.addEventListener('click', this.prev.bind(this));
+    }
+
+    next () {
+        this.goToItem(this.currentItem + this.options.slidesToScroll);
+    }
+    prev () {
+        this.goToItem(this.currentItem - this.options.slidesToScroll);
+    }
+
+    //Move carousel toward target element
+    //{number} index
+    goToItem (index) {
+        if(index<0) {
+            index = this.items.length - this.options.slidesVisible
+        }else if(index >= this.items.length || this.items[this.currentItem + this.options.slidesVisible] === undefined) {
+            index=0;
+        }
+        let translateX = index * -100 / this.items.length
+        this.container.style.transform = 'translate3d(' + translateX + '%, 0, 0)';
+        this.currentItem = index;
+    }
+    // {string} className
+    //return : {HTMLelement}
+    createDivWithClass (className) {
+        let div = document.createElement('div');
+        div.setAttribute('class', className);
+        return div
+    }
+};
+
+document.addEventListener('DOMContentLoaded', function (){
+    new Carousel(document.querySelector('#carousel1'), {
+        slidesToScroll: 1,
+        slidesVisible: 2
+    })
+})
---
+++ js/index.js
@@ -0,0 +1,106 @@
+
+// Competence array
+let competenceLogoClass = [
+    "fa-html5",
+    "fa-css3-alt",
+    "fa-sass",
+    "fa-bootstrap",
+    "fa-js",
+    "fa-vuejs",
+    "fa-node",
+    "fa-npm",
+    "fa-git",
+    "fa-github"
+]
+let competenceLogoClassColor = [
+    "html",
+    "css",
+    "sass",
+    "bootstrap",
+    "javascript",
+    "vuejs",
+    "nodejs",
+    "npm",
+    "git",
+    "github"
+]
+let skillName = [
+    "HTML5",
+    "CSS3",
+    "Sass",
+    "Bootstrap",
+    "JavaScript",
+    "Vue.js",
+    "node.js",
+    "npm",
+    "git",
+    "Github"
+]
+let competenceImage = [
+
+]
+let arraySkill = []
+let levelSkills = []
+
+
+// Skills logo
+
+
+const langageCompetence = document.getElementById("langage-competence");
+langageCompetence.classList.add("card-group", "langage-competence");
+let divCard = document.createElement("div");
+    langageCompetence.appendChild(divCard);
+    divCard.classList.add("row", "row-cols-1", "row-cols-md-2","row-cols-lg-4" );
+
+for (let i = 0; i < competenceLogoClass.length; i++) {
+    let divCardCard = document.createElement("div");
+    divCard.appendChild(divCardCard);
+    divCardCard.classList.add("col-md-4", "col-lg-2");
+
+    let card = document.createElement("div");
+    divCardCard.appendChild(card);
+    card.classList.add("card", "m-1");
+
+    let cardBody = document.createElement("div");
+    card.appendChild(cardBody);
+    cardBody.classList.add("card-body", "text-center");
+
+    let logoCompetence = document.createElement("i");
+    cardBody.appendChild(logoCompetence);
+    logoCompetence.classList.add("fab", competenceLogoClass[i], "logo-langage", competenceLogoClassColor[i], "card-title"); 
+    arraySkill.push(logoCompetence);
+
+    let nameSkill = document.createElement("div");
+    cardBody.appendChild(nameSkill);
+    nameSkill.innerHTML = skillName[i]
+    nameSkill.classList.add("card-title","skill-name")
+
+}
+
+// //Show Level
+// console.log(arraySkill);
+// for (let i = 0; i < arraySkill.length; i++) {
+//     arraySkill[i].addEventListener('mouseover', function () {
+//         console.log(arraySkill[i])
+//         // langageCompetence.removeChild(arraySkill[i]);
+//         let levelSkill = document.createElement('p');
+//         // langageCompetence.appendChild(levelSkill);
+//         langageCompetence.replaceChild(levelSkill, arraySkill[i])
+//         levelSkill.innerHTML="coucou"
+//         // supprimer le logo et ajouter un texte ou le level
+//         levelSkills.push(levelSkill);
+//     })
+// }
+// console.log(levelSkills)
+// for (let i = 0; i < levelSkills.length; i++){
+//     console.log('boucle')
+//     levelSkills[i].addEventListener('mouseout', function () {
+//         console.log('event')
+//         // langageCompetence.appendChild(levelSkill);
+//         for (let j = 0; j < arraySkill.length; j++){
+//         langageCompetence.replaceChild(arraySkill[j],levelSkills[i])
+//         }
+//          // supprimer le logo et ajouter un texte ou le level
+//     })
+//     // })
+// }
